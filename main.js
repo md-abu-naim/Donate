0626c2f645a68c88022eb2521193e255e8119463
abunaim34 <farhanadnanfarabi@gmail.com>
2025-03-10 14:17:09 +0600

    noakhali donation carder funtionality is okay

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const donationPart = document.getElementById('donation')
-const HistoryBtn = document.getElementById('btn-history')
-const donationBtn = document.getElementById('btn-donation')
 const HistoryPart = document.getElementById('history')
+const donationBtn = document.getElementById('btn-donation')
+const HistoryBtn = document.getElementById('btn-history')
+const ammount = document.getElementById('balance')
+const balance = parseFloat(ammount.innerText)
 
+// history Button
 const btnHistory = document.getElementById('btn-history').addEventListener('click', function () {
     donationPart.classList.add('hidden')
     HistoryBtn.classList.add('btn-history')
@@ -10,6 +13,38 @@ const btnHistory = document.getElementById('btn-history').addEventListener('clic
     HistoryPart.classList.remove('hidden')
 })
 
-donationBtn.addEventListener('click', function () {
 
+// Donation Button
+donationBtn.addEventListener('click', function () {
+    donationBtn.classList.remove('btn-donation')
+    HistoryBtn.classList.remove('btn-history')
+    donationPart.classList.remove('hidden')
+    HistoryPart.classList.add('hidden')
+})
+
+// Noakhali Donate Card
+document.getElementById('noakhali').addEventListener('click', function () {
+    const inputField = document.getElementById('noakhali-input')
+    const inputText = inputField.value
+
+    const addElement = document.getElementById('noakhali-donate')
+    const addDonate = parseFloat(addElement.innerText) || 0
+    const inputAmount = parseFloat(inputText)
+
+    const total = addDonate + inputAmount
+
+    addElement.innerText = total
+
+    const newBalance = balance - inputAmount
+    document.getElementById('balance').innerText = newBalance
+
+    const div = document.createElement('div')
+    div.classList.add('history-box')
+
+    div.innerHTML = `<h4><span>${total}</span> Taka is Donated for famine-2024 at Feni, Bangladesh</h4>
+                <p class="histroy-title">Date : ${new Date()} </p>`
+
+    HistoryPart.append(div)
+    console.log(HistoryPart);
+    
 })
