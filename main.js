456e8c5817be713ee6add68096adf0cb6e74d1c3
abunaim34 <farhanadnanfarabi@gmail.com>
2025-03-10 13:07:28 +0600

    its complate

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const donationPart = document.getElementById('donation')
 const HistoryBtn = document.getElementById('btn-history')
 const donationBtn = document.getElementById('btn-donation')
+const HistoryPart = document.getElementById('history')
 
 const btnHistory = document.getElementById('btn-history').addEventListener('click', function () {
     donationPart.classList.add('hidden')
     HistoryBtn.classList.add('btn-history')
-    donationBtn.classList.add('remove')
+    donationBtn.classList.add('btn-donation')
+    HistoryPart.classList.remove('hidden')
+})
+
+donationBtn.addEventListener('click', function () {
 
 })
